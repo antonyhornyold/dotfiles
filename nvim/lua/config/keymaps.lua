38c22587cb2bf7b267ef6c4732184e5ce28cfb42
Antony Hornyold <1015546+antonyhornyold@users.noreply.github.com>
2026-09-20 21:49:27 +0100

    Remove unused Lua reload keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,8 +1,3 @@
--- Save and re-run current Lua file do not use for statusline.lua
-vim.keymap.set("n", "<leader>rr", "<cmd>update<CR><cmd>luafile %<CR>", {
-  desc = "Save and reload current Lua file",
-})
-
 -- Press jk in insert mode to return to normal mode.
 vim.keymap.set("i", "jk", "<Esc>", {
   desc = "Exit insert mode",
