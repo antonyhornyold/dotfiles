98e5cc7caf39e3de216dfe1f9b987daf26452329
Antony Hornyold <1015546+antonyhornyold@users.noreply.github.com>
2026-09-20 21:02:27 +0100

    Configure formatting for web files and Lua

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -11,3 +11,7 @@ vim.keymap.set("i", "jk", "<Esc>", {
 vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", {
   desc = "Open floating file explorer",
 })
+
+vim.keymap.set({ "n", "v" }, "<leader>f", function()
+  require("conform").format({ async = true })
+end, { desc = "Format buffer or selection" })
--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -7,6 +7,7 @@ vim.pack.add({
   "https://github.com/mason-org/mason.nvim",
   "https://github.com/neovim/nvim-lspconfig",
   "https://github.com/mason-org/mason-lspconfig.nvim",
+  "https://github.com/stevearc/conform.nvim",
 })
 
 require("mini.icons").setup()
