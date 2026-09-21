4604d16ab3db49ba18362499c6a507b96ba167cb
Antony Hornyold <1015546+antonyhornyold@users.noreply.github.com>
2026-09-21 20:32:19 +0100

    Add SuperTab completion and snippet mappings

--- nvim/lua/config/completion.lua
+++ nvim/lua/config/completion.lua
@@ -15,3 +15,33 @@ snippets.setup({
 
 snippets.start_lsp_server({ match = false })
 require("mini.completion").setup()
+
+local map_multistep = require("mini.keymap").map_multistep
+
+-- Preserve indentation before any text, unless a snippet is already active.
+local indent_before_text = {
+  condition = function()
+    if snippets.session.get() then return false end
+    local col = vim.api.nvim_win_get_cursor(0)[2]
+    local before_cursor = vim.api.nvim_get_current_line():sub(1, col)
+    return before_cursor:match("^%s*$") ~= nil
+  end,
+  action = function() return "<Tab>" end,
+}
+
+map_multistep("i", "<Tab>", {
+  indent_before_text,
+  "pmenu_next",
+  "minisnippets_next",
+  "minisnippets_expand",
+}, { desc = "Indent, select completion, or advance snippet" })
+
+map_multistep("i", "<S-Tab>", {
+  "pmenu_prev",
+  "minisnippets_prev",
+}, { desc = "Previous completion or snippet placeholder" })
+
+map_multistep("i", "<CR>", {
+  "pmenu_accept",
+  "minipairs_cr",
+}, { desc = "Accept completion or insert newline" })
