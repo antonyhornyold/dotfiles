b33c511c387df9e745c4c305c92eb695a1f050b6
Antony Hornyold <1015546+antonyhornyold@users.noreply.github.com>
2026-09-21 20:15:24 +0100

    Add mini.snippets completion and language snippets

--- nvim/lua/config/completion.lua
+++ nvim/lua/config/completion.lua
@@ -1,1 +1,17 @@
+local snippets = require("mini.snippets")
+
+snippets.setup({
+  snippets = {
+    snippets.gen_loader.from_file(vim.fn.stdpath("config") .. "/snippets/global.json"),
+    snippets.gen_loader.from_lang(),
+  },
+  mappings = {
+    expand = "<C-j>",
+    jump_next = "<C-l>",
+    jump_prev = "<C-h>",
+    stop = "<C-c>",
+  },
+})
+
+snippets.start_lsp_server({ match = false })
 require("mini.completion").setup()
