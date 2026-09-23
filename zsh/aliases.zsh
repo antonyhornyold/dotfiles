59c5637715bb4312b020acb1b851ee05145c178e
Antony Hornyold <1015546+antonyhornyold@users.noreply.github.com>
2026-09-23 10:18:21 +0100

    small addition to ls

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,10 +1,10 @@
 # Modern directory listings with concise and detailed variants.
-if (( $+commands[eza] )); then
+if (($+commands[eza])); then
   # Catppuccin Mocha: text for files, blue for directories, green for executables,
   # flamingo for project files, mauve for source, and lavender for compiled files.
   export EZA_COLORS='fi=38;2;205;214;244:di=38;2;137;180;250:ex=38;2;166;227;161:bu=38;2;242;205;205:sc=38;2;203;166;247:cm=38;2;180;190;254'
 
-  alias ls='eza --color=auto --icons=auto'
+  alias ls='eza -1h --color=auto --icons=auto --git'
   alias ll='eza --color=auto --long --git --no-filesize --icons=auto --no-time --no-user --no-permissions'
   alias la='eza --color=auto --long --git --no-filesize --icons=auto --no-time --no-user --no-permissions --all'
   alias tr1='eza --color=auto --tree --level=2 --git-ignore --git --icons=auto --all'
